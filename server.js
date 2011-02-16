dc2ff5a88456843d05d333c8894e2af6e456d0d1
Jeff Remer <jeff@threestarchina.com>
2011-02-15 17:43:30 -0800

    Fixed missing space in log and changed ot console.log instead of sys.log

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ http.createServer(function(request, response) {
 					Script.runInNewContext(urlCode, urlSandbox);
 				}
 				sourceUrl = urlSandbox.URL;
-				console.log("Source URL" + sourceUrl);
+				console.log("Source URL " + sourceUrl);
 			} catch (err) {
 				console.log("Problem running code on sourceUrl\n" + err);
 			}
@@ -120,5 +120,5 @@ function log(statCode, url, ip,err) {
   var logStr = statCode + ' - ' + url + ' - ' + ip
   if (err)
     logStr += ' - ' + err;
-  sys.log(logStr);
+  console.log(logStr);
 }
